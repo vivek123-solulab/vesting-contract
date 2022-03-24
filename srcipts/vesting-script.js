d0675f689b5fa1c1370f15a916553fc9ef81140a
vivek123-solulab <101550880+vivek123-solulab@users.noreply.github.com>
2022-03-24 00:14:36 -0700

    updated script.js

--- srcipts/vesting-script.js
+++ srcipts/vesting-script.js
@@ -1,8 +1,12 @@
 async function main() {
-    const Raffle = await ethers.getContractFactory("raffle");
-    const raffle = await Raffle.deploy();
-    await raffle.deployed();
-    console.log("Raffle deployed to:", raffle.address);
+    const TokenVesting = await ethers.getContractFactory("TokenVesting");
+    const tokenVesting = await TokenVesting.deploy();
+    await tokenVesting.deployed();
+    const MyToken = await ethers.getContractFactory("TokenVesting");
+    const myToken = await MyToken.deploy();
+    await myToken.deployed();
+    console.log("MyToken contract deployed to:", myToken.address);
+    console.log("Vesting contract deployed to:", tokenVesting.address);
 }
 main()
     .then(() => process.exit(0))
